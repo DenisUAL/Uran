b41c7ef89214f96d3eedd741e5f0f098dbc0c204
DenisUAL <denisandreev64@gmail.com>
2017-12-29 07:59:20 -0500

    env variable added

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
   })
 })
 
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
