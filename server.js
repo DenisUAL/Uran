6cb2c52699aebcee4c4a076a22157450452fe15b
DenisUAL <denisandreev64@gmail.com>
2017-12-29 11:43:42 -0500

    location

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
-const geoip2 = require('geoip2');
-geoip2.init();
+const iplocation = require('iplocation');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const userRecord = require('./db.js');
@@ -12,29 +11,27 @@ const userRecord = require('./db.js');
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 app.use(volleyball);
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/api', require('./api/router.js'))
 
 app.get('/', (req, res) => {
-  userRecord.find()
-  .then((data) => {
-    let location;
-    const IP = req.ip;
-    console.log(IP, 'this is IP <-----------')
-    geoip2.lookupSimple(IP, function(error, result) {
-      if (error) {
-        console.log("Error: %s", error);
-      }
-      else if (result) {
-        location = result;
-        console.log(location);
-      }
-    });
-    res.render('index', {data});
-  })
+  userRecord
+    .find()
+    .then((data) => {
+      let location;
+      const IP = req.ip;
+      console.log(IP, 'this is IP <-----------')
+      iplocation(IP, function (error, res) {
+        if (error) {
+          console.log("Error: %s", error);
+        } else if (res) {
+          location = res;
+          console.log(location);
+        }
+      })
+      res.render('index', {data});
+    })
 })
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test');
